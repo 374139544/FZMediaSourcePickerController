6dd988df9eeb83a1c75dadfc7f23c663b776fe51
fengzhao <374139544@qq.com>
2017-05-19 13:33:40 +0800

    添加 UIButton+PHAsset

---
+++ FZMediaSourcePickerController/FZMediaSourcePickerController/Category/UIButton+PHAsset.h
@@ -0,0 +1,26 @@
+//
+//  UIButton+PHAsset.h
+//  ninth
+//
+//  Created by fengzhao on 2017/5/19.
+//  Copyright © 2017年 ninth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (PHAsset)
+
+- (void)fz_setImageWithPHAsset:(PHAsset *)asset forState:(UIControlState)state;
+- (void)fz_setImageWithPHAsset:(PHAsset *)asset placeholderImage:(UIImage *)placeholderImage forState:(UIControlState)state;
+
+- (void)fz_setThumbnailImageWithPHAsset:(PHAsset *)asset thumbnailImageSize:(CGSize)size forState:(UIControlState)state;
+- (void)fz_setThumbnailImageWithPHAsset:(PHAsset *)asset thumbnailImageSize:(CGSize)size placeholderImage:(UIImage *)placeholderImage forState:(UIControlState)state;
+
+
+- (void)fz_setBackgroundImageWithPHAsset:(PHAsset *)asset forState:(UIControlState)state;
+- (void)fz_setBackgroundImageWithPHAsset:(PHAsset *)asset placeholderImage:(UIImage *)placeholderImage forState:(UIControlState)state;
+
+- (void)fz_setBackgroundThumbnailImageWithPHAsset:(PHAsset *)asset thumbnailImageSize:(CGSize)size forState:(UIControlState)state;
+- (void)fz_setBackgroundThumbnailImageWithPHAsset:(PHAsset *)asset thumbnailImageSize:(CGSize)size placeholderImage:(UIImage *)placeholderImage forState:(UIControlState)state;
+
+@end
